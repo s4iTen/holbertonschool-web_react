387a749c2243f440f3812e190973b1030c6d2fda
s4iTen <ychouk102@gmail.com>
2024-04-30 21:53:59 +0200

    this is the task 2 updates

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
+import './App.css'
 
 function App() {
   return (
-    <div>
+    <div className="div">
       <div className="App-header">
         <img src="holbimg.jpg" alt="Holberton-Logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <div className="Flex">
         <div>
           <label htmlFor="email">Email:</label>
           <input type="email" id="email" />
@@ -18,6 +20,7 @@ function App() {
           <input type="password" id="password" />
         </div>
         <button>OK</button>
+        </div>
       </div>
       <div className="App-footer">
         <p>Copyright 2020 - Holberton School</p>
