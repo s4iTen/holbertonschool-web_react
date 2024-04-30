74d27ba8da00b65700c81b6274063910e3389d65
s4iTen <ychouk102@gmail.com>
2024-04-30 21:00:23 +0200

    this is the last updates

--- react_intro/task_2/dashboard/src/Notifications.js
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { getLatestNotification } from './utils';
 import './Notifications.css';
-
 function Notifications() {
+  const handleButtonClick = () => {
+    console.log('Close button has been clicked');
+  };
+
   return (
-    <div>
-        <p>Here is the list of notifications</p>
+    <div className="Notifications">
+      <p>Here is the list of notifications</p>
+      <button
+        style={{
+          float: 'right',
+          backgroundColor: 'transparent',
+          border: 'none',
+          cursor: 'pointer',
+        }}
+        aria-label="Close"
+        onClick={handleButtonClick}
+      >
+        x
+      </button>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Notifications
+export default Notifications;
--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -4,3 +4,6 @@ export function getFullYear() {
 export function getFooterCopy(isIndex) {
     return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
 }
+export function getLatestNotification() {
+    return "<strong>Urgent requirement</strong> - complete by EOD";
+  }
