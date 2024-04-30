fc7ac18bbb6f08cbd1ad3b35d5f31e446eab9a7c
s4iTen <ychouk102@gmail.com>
2024-04-30 13:15:56 +0200

    this is the last updates of creating notification and utils and two functions in the index.js

---
+++ react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Notifications.css';
+
+function Notifications() {
+  return (
+    <div>
+        <p>Here is the list of notifications</p>
+    </div>
+  )
+}
+
+export default Notifications
