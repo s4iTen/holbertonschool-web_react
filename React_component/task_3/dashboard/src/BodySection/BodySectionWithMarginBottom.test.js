d3d183ff0faaf22264a36c6b1f7450b48f87c359
s4iTen <ychouk102@gmail.com>
2024-05-10 13:58:36 +0200

    this is the clean code

--- React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
+++ React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { BodySection } from './BodySection';
 import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
 
 describe('<BodySectionWithMarginBottom />', () => {
