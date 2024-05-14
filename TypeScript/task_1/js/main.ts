e96148ee5dc8cd5ad2273d51aed90e60ba1a92fb
s4iTen <ychouk102@gmail.com>
2024-05-14 18:23:38 +0200

    this is the last updates

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -25,3 +25,30 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 console.log(printTeacher(teacher3.firstName, teacher3.lastName));
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
