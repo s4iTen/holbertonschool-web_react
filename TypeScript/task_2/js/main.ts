7ba7d3f0d29d14a3550ef59d6794366a238694a3
s4iTen <ychouk102@gmail.com>
2024-05-15 12:27:53 +0200

    this is the last updates

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -64,3 +64,18 @@ function executeWork(employee: Director | Teacher) {
 
 executeWork(createEmployee(200));
 executeWork(createEmployee(1000));
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    } else if (todayClass === 'History') {
+        return 'Teaching History';
+    } else {
+        throw new Error('Invalid subject');
+    }
+}
+
+console.log(teachClass('Math')); // Output: Teaching Math
+console.log(teachClass('History')); // Output: Teaching History
