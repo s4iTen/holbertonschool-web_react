84ee471bbf1266d0cf18030373913a212a0c868c
s4iTen <ychouk102@gmail.com>
2024-05-16 14:42:57 +0200

    this is the last updates

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,9 +1,17 @@
 import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
+import logoImage from '../assets/holberton-logo.jpg';
 
 $(document).ready(function() {
-    $('body').append("<div id='logo'></div>");
+    $('body').append(`<div id="logo"></div>`); // Add the logo element at the top of the document
+    $('#logo').css({
+        width: '200px',
+        height: '200px',
+        backgroundImage: `url(${logoImage})`, // Set the background image of the logo
+        backgroundSize: 'cover',
+    });
+
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<button id="clickButton">Click here to get started</button>');
