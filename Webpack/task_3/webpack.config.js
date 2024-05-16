ea9e7bd85efa40f93e933d5b974e56be38d6374d
s4iTen <ychouk102@gmail.com>
2024-05-16 15:09:52 +0200

    this is the last updates

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: {
         header: './modules/header/header.js',
         body: './modules/body/body.js',
