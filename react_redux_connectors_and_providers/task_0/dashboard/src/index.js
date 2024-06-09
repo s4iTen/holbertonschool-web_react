e456e38285a823e8405cc6869c61f72d1165e578
s4iTen <ychouk102@gmail.com>
2024-06-09 03:54:04 +0200

    Added Redux store and provided it to the App

--- react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from "./App/App";
 import AppContext from "./App/AppContext";
+import uiReducer from './reducers/uiReducer'; // replace with the actual path to your uiReducer
 import reportWebVitals from "./reportWebVitals";
 
 const actualUser = {
@@ -14,11 +17,15 @@ const actualLogOut = () => {
   console.log('Actual user logged out');
 };
 
+let store = createStore(uiReducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <AppContext.Provider value={{ user: actualUser, logOut: actualLogOut }}>
-      <App />
-    </AppContext.Provider>
+    <Provider store={store}>
+      <AppContext.Provider value={{ user: actualUser, logOut: actualLogOut }}>
+        <App />
+      </AppContext.Provider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
